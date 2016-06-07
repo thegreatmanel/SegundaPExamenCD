140b412e57669fefd2a6c71473fe2070190da340
damain <damian@danielcastelao.org>
2016-06-07 20:18:48 +0200

    Inicio

---
+++ src/busqueda/Busqueda.java
@@ -0,0 +1,33 @@
+package busqueda;
+
+public class Busqueda {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[100];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i * 5;
+        }
+        Busqueda ejemplo = new Busqueda();
+        int indice = ejemplo.busquedaBinaria(numeros, 450, 0, numeros.length - 1);
+        System.out.println("El indice del valor '450' es: " + indice);
+    }
+
+    public int busquedaBinaria(int[] listado, int clave, int posInicial, int posFinal) {
+        int posMitad = (posFinal + posInicial) / 2;
+        if (clave == listado[posMitad]) {
+            return posMitad;
+        } else if (clave < listado[posMitad]) {
+            if (posMitad - 1 <= posInicial) {
+                return -1;
+            } else {
+                return busquedaBinaria(listado, clave, posInicial, posMitad - 1);
+            }
+        } else {
+            if (posMitad + 1 >= posFinal) {
+                return -1;
+            } else {
+                return busquedaBinaria(listado, clave, posMitad + 1, posFinal);
+            }
+        }
+    }
+}
